9b6ae6769ac2b67a13c9b1bd2021bafb9f3f1b6f
Daniel Heringer <danielheringers@icloud.com>
2022-12-02 16:04:53 -0300

    entrega finalizada

--- selects.sql
+++ selects.sql
@@ -2,11 +2,38 @@
 -- Seleções de dados
 
 -- 1)
-
+ SELECT pedidos.id, pedidos.status, pedidos.cliente_id, produtos.id, produtos.nome, produtos.tipo, produtos.preço, produtos.pts_de_lealdade
+ FROM produtos_pedidos
+ INNER JOIN produtos ON produtos_pedidos.produto_id = produtos.id
+ INNER JOIN pedidos  ON produtos_pedidos.pedido_id = pedidos.id
+ INNER JOIN clientes ON pedidos.cliente_id = clientes.id;
+ 
 -- 2)
-
+ SELECT pedidos.id
+ FROM produtos_pedidos
+ INNER JOIN produtos ON produtos_pedidos.produto_id = produtos.id
+ INNER JOIN pedidos  ON produtos_pedidos.pedido_id = pedidos.id
+ INNER JOIN clientes ON pedidos.cliente_id = clientes.id
+ WHERE produtos.nome = 'Fritas';
 -- 3)
-
+ SELECT clientes.nome AS gostam_de_fritas
+ FROM produtos_pedidos
+ INNER JOIN produtos ON produtos_pedidos.produto_id = produtos.id
+ INNER JOIN pedidos  ON produtos_pedidos.pedido_id = pedidos.id
+ INNER JOIN clientes ON pedidos.cliente_id = clientes.id
+ WHERE produtos.nome = 'Fritas';
+ ;
 -- 4)
-
+ SELECT sum(produtos.preço) AS total_laura
+ FROM produtos_pedidos
+ INNER JOIN produtos ON produtos_pedidos.produto_id = produtos.id
+ INNER JOIN pedidos  ON produtos_pedidos.pedido_id = pedidos.id
+ INNER JOIN clientes ON pedidos.cliente_id = clientes.id
+ WHERE clientes.nome = 'Laura';
 -- 5)
+ SELECT produtos.nome, count(produtos_pedidos.produto_id)
+ FROM produtos_pedidos
+ INNER JOIN produtos ON produtos_pedidos.produto_id = produtos.id
+ INNER JOIN pedidos  ON produtos_pedidos.pedido_id = pedidos.id
+ INNER JOIN clientes ON pedidos.cliente_id = clientes.id
+GROUP BY produtos.nome;
--- tables.sql
+++ tables.sql
@@ -1,14 +1,45 @@
 -- Aqui você deve colocar os códigos SQL referentes à
 
 -- Criação das tabelas
+CREATE  TABLE IF NOT EXISTS clientes(
+	id BIGSERIAL CONSTRAINT pk_clientes PRIMARY KEY,
+	nome VARCHAR(50) NOT NULL,
+	lealdade INTEGER NOT NULL
+);
 
--- Tabela clientes
+CREATE TABLE IF NOT EXISTS endereços(
+	id BIGSERIAL CONSTRAINT pk_enderecos PRIMARY KEY,
+	cep VARCHAR(9) NOT NULL,
+	rua VARCHAR(50) NOT NULL,
+	numero INTEGER NOT NULL,
+	bairro VARCHAR(50) NOT NULL,
+	complemento VARCHAR(100),
+	cliente_id INTEGER UNIQUE NOT NULL,
+	CONSTRAINT fk_cliente_id FOREIGN KEY (cliente_id) REFERENCES clientes(id)
+		ON DELETE CASCADE
+);
+CREATE TABLE IF NOT EXISTS pedidos(
+	id BIGSERIAL CONSTRAINT pk_pedidos PRIMARY KEY,
+	status VARCHAR(50) NOT NULL,
+	cliente_id INTEGER NOT NULL,
+	CONSTRAINT fk_cliente_id FOREIGN KEY (cliente_id) REFERENCES clientes(id)
+		ON DELETE CASCADE
+);
 
--- Tabela endereços
+CREATE TABLE IF NOT EXISTS produtos(
+	id BIGSERIAL CONSTRAINT pk_produtos PRIMARY KEY,
+	nome VARCHAR(100) UNIQUE NOT NULl,
+	tipo VARCHAR(30) NOT NULL,
+	preço FLOAT(8) NOT NULL,
+	pts_de_lealdade INTEGER NOT NULL
+);
 
--- Tabela pedidos
-
--- Tabela produtos
-
--- Tabela produtos_pedidos
+CREATE TABLE IF NOT EXISTS produtos_pedidos(
+	id BIGSERIAL CONSTRAINT pk_produtos_pedidos PRIMARY KEY,
+	pedido_id INTEGER NOT NULL,
+	produto_id INTEGER NOT NULL,
+	CONSTRAINT fk_pedido_id FOREIGN KEY (pedido_id) REFERENCES pedidos(id) ON DELETE CASCADE,
+	CONSTRAINT fk_produto_id FOREIGN KEY (produto_id) REFERENCES produtos(id)
+		ON DELETE CASCADE
+);
 
